5556806a78f378aa7f9ee4591ba42baad5563fa1
jpap1021 <juan.arteaga8710@gmail.com>
2019-08-25 10:21:15 -0500

    header

--- src/app/header/header.js
+++ src/app/header/header.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import './header.css'
 class Header extends React.Component{
     render(){
         return(
-          <div>
-              Header
+          <div className="header-component">
+              <div className="title-home">
+              <a href="#">Home</a>
+              </div>
+              <div className="title-listfavorites">
+              <a href="#">List favorites</a>
+              </div>
           </div>
 
         );
